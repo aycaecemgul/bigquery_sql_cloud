86a5dee71cb01a99acd51e2b5e8d9e87ec4bda61
aycaecemgul <aycaecemgul@gmail.com>
2020-08-24 22:28:43 +0300

    c

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import os
 import pyarrow
+from google.cloud import bigquery
 from google.cloud.bigquery.client import Client
 from google.cloud.bigquery.dataset import TableReference,DatasetReference
 from google.cloud import bigquery_storage
@@ -13,4 +14,13 @@ dataset = client.get_dataset(dataset_ref)
 table_ref = TableReference(dataset_ref=dataset_ref,table_id="full")
 table= client.get_table(table_ref)
 
-client.list_rows(table, selected_fields=table.schema[:1], max_results=5).to_dataframe()
+
+def estimate_query_size(query):
+    # Create a QueryJobConfig object to estimate size of query without running it
+    dry_run_config = bigquery.QueryJobConfig(dry_run=True)
+
+    # API request - dry run query to estimate costs
+    dry_run_query_job = client.query(query, job_config=dry_run_config)
+
+    print("This query will process {} bytes.".format(dry_run_query_job.total_bytes_processed))
+
